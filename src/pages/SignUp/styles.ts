ab2cc9ad7656363d61b74b071e59a302a7cecd53
Fernando Kenji <kenjifernandoo@gmail.com>
2020-07-03 18:53:58 -0300

    Initial commit

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,122 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #212329;
+      border-radius: 10px;
+      border: 2px solid #212329;
+      padding: 16px;
+      width: 100%;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #636360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
